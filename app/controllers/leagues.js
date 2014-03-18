9cfc0bb4c4bfce79b4f889d51a7c7edffffce22b
Andrew Magill <andrew.b.magill@gmail.com>
2014-03-18 01:06:17 -0500

    mongoose has an e

--- app/controllers/leagues.js
+++ app/controllers/leagues.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
   , async = require('async')
-  , League = mongoos.model('League')
+  , League = mongoose.model('League')
   , _ = require('underscore')
 
 exports.create = function (req, res) {
@@ -10,47 +10,47 @@ exports.create = function (req, res) {
   res.jsonp(league)
 }
 
-exports.show = function(req, res) {
-  res.jsonp(req.league);
+exports.show = function (req, res) {
+  res.jsonp(req.league)
 }
 
-exports.league = function(req, res, next, id) {
+exports.league = function (req, res, next, id) {
   var League = mongoose.model('League')
 
-  League.load(id, function (err, league) {
+  League.load(id, function (err, league){
     if (err) return next(err)
-    if(!League) return next(new Error('Failed to load league ' + id))
+    if (!league) return next (new Error('Failed to load league ' + id))
     req.league = league
     next()
-  })
+  });
 }
 
-exports.all = function(req, res) {
-  League.find().populate('commissioner').exec(function(err, leagues) {
-    if (err) {
-      res.render('error', (status: 500});
-    } else {
-        res.jsonp(leagues);
+exports.all = function (req, res) {
+  League.find().populate('commissioner').exec(function (err, leagues) {
+    if(err) {
+      res.render('error', {status: 500});
+    }else{
+      res.jsonp(leagues);
     }
   });
 }
 
-exports.update = function(req, res) {
+exports.update = function (req, res) {
   var league = req.league
   league = _.extend(league, req.body)
 
   league.save(function(err) {
     res.jsonp(league)
-  })
+  });
 }
 
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   var league = req.league
-  league.remove(function(err) {
-    if (err) {
-      res.render('error', {status: 500});
+  league.remove(function (err) {
+    if (err){
+      res.render('error', {status:500});
     } else {
       res.jsonp(1);
     }
-  })
+  });
 }
